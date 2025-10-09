71ade607bb6660298d6cd89d706790c47602ef1b
eduardo <eduardomendes418@gmail.com>
2025-10-09 13:19:36 -0300

    [ECM-17] refactor -  layout

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -23,23 +23,23 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ClerkProvider>
           <div className="mx-auto p-4 sm:px-0 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
             <Navbar />
             {children}
             <Footer />
           </div>
           <ToastContainer position="bottom-right" />
-        </body>
-      </html>
-    </ClerkProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
