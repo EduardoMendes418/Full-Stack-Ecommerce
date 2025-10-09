fc7fcd69b3ea747b1bebbf7e0341df8c71f55c49
eduardo <eduardomendes418@gmail.com>
2025-10-09 15:53:08 -0300

    [ECM-21] feat - create product-service auth

--- apps/product-service/index.ts
+++ apps/product-service/index.ts
@@ -1,25 +1,35 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import 'dotenv/config'
+import { requireAuth } from "@clerk/express";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["http://localhost:3002", "http://localhost:3003"],
-    credentials: true,
-  })
-);
+app.use(cors({ origin: ["http://localhost:3002", "http://localhost:3003"], credentials: true }));
+app.use(express.json());
+app.use(clerkMiddleware())
+
+declare global {
+  namespace Express {
+    interface Request {
+      auth?: any;
+    }
+  }
+}
 
 app.get("/health", (req: Request, res: Response) => {
-  res.status(200).json({
-    status: "ok",
-    uptime: process.uptime(),
-    timeStamp: Date.now(),
+  res.json({ status: "ok", timestamp: Date.now() });
+});
+
+app.get("/test", requireAuth(), (req, res) => {
+  const auth = req.auth(); 
+  console.log("User ID:", auth.userId);
+
+  res.json({
+    message: "Rota protegida acessada",
+    userId: auth.userId,
   });
 });
 
-app.listen(8000, () => {
-  console.log("product service is running on port 8000");
-});
-
-app.use(express.json());
+app.listen(8000, () => console.log("✅ Backend rodando na porta 8000"));
