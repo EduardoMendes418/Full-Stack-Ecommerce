f70fc896df5c5f82386baafe6b04e3d96f96e233
eduardo <eduardomendes418@gmail.com>
2025-11-08 11:50:29 -0300

    [ECM-29] fix - adjusting index order

---
+++ apps/order-service/src/index.ts
@@ -0,0 +1,55 @@
+import "dotenv/config.js";
+import Fastify from "fastify";
+import Clerk from "@clerk/fastify";
+import cors from "@fastify/cors";
+
+
+const fastify = Fastify();
+fastify.register(Clerk.clerkPlugin, {
+  secretKey: process.env.CLERK_SECRET_KEY!,
+});
+
+fastify.register(cors, {
+  origin: ["http://localhost:3002"],
+  credentials: true,
+});
+
+fastify.get("/health", (request, reply) => {
+  return reply.status(200).send({
+    status: "ok",
+    uptime: process.uptime(),
+    timeStamp: Date.now(),
+  });
+});
+
+fastify.get("/auths/product-page", async (request, reply) => {
+  try {
+    const { userId } = Clerk.getAuth(request);
+
+    if (!userId) {
+      return reply.status(401).send({ message: "❌ Não logado" });
+    }
+
+    console.log("User ID:", userId);
+
+    return reply.send({
+      message: "✅ Order Service autenticado",
+      userId,
+    });
+  } catch (err) {
+    console.error("❌ Erro no Order Service:", err);
+    return reply.status(500).send({ message: "Erro interno do Order Service" });
+  }
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8001 });
+    console.log("✅ Order Service is running on port 8001");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
